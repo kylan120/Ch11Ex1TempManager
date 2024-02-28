86dd505417f5a930dccd48e82b6b12e44ca03bbc
kylan120 <142834131+kylan120@users.noreply.github.com>
2024-02-28 09:20:32 -0600

    did steps 7-9

--- TempManager/Models/Temp.cs
+++ TempManager/Models/Temp.cs
@@ -1,12 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TempManager.Models
 {
     public class Temp
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a valid date")]
         public DateTime? Date { get; set; }
+       
+        [Range (-200, 200)]
         public double? Low { get; set; }
+        
+        [Range(-200, 200)]
         public double? High { get; set; }
     }
 }
